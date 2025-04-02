a26987f5372f79048d9c8c471d7f6d88105b5e12
siddheshpagar <pagarsiddhesh2000@gmail.com>
2025-04-02 18:42:07 +0530

    Frontend is running smoothly 🚀 | All features working as expected ✅

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,11 @@
+import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 
 // Middleware function
 export function middleware(request) {
-    const userToken = request.cookies.get("userToken")?.value;
-    const adminToken = request.cookies.get("adminToken")?.value;
+    const cookieStore = cookies();
+    const userToken = cookieStore.get("userToken")?.value;
+    const adminToken = cookieStore.get("adminToken")?.value;
 
     const userPaths = ['/user/login', '/user/signup'];
     const adminPaths = ['/admin/login', '/admin/signup'];
@@ -20,26 +22,27 @@ export function middleware(request) {
     // User logic
     if (isUserPath) {
         if (userToken) {
-            console.log('Authenticated user attempting to access login/signup.');
+            // console.log('Authenticated user attempting to access login/signup.');
             return NextResponse.redirect(new URL('/', request.url));
         }
     } else if (!userToken && !isAdminRoute) {
-        console.log('Unauthenticated user attempting to access secured route.');
+        // console.log('Unauthenticated user attempting to access secured route.');
         return redirectToLogin('/user/login');
     }
 
     // Admin logic
     if (isAdminPath) {
         if (adminToken) {
-            console.log('Authenticated admin attempting to access login/signup.');
+            // console.log('Authenticated admin attempting to access login/signup.');
             return NextResponse.redirect(new URL('/admin/dashboard/users', request.url));
         }
     } else if (!adminToken && isAdminRoute) {
-        console.log('Unauthenticated admin attempting to access secured admin route.');
+        // console.log('Unauthenticated admin attempting to access secured admin route.');
         return redirectToLogin('/admin/login');
     }
 
-    console.log(`Middleware: UserToken=${userToken}, AdminToken=${adminToken}`);
+    // console.log(`Middleware: UserToken=${userToken}, AdminToken=${adminToken}`);
+    return NextResponse.next();
 }
 
 // Configuration for matching paths
