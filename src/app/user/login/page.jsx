393fa1908999e12db7a052f87584df5d59a3f803
siddheshpagar <pagarsiddhesh2000@gmail.com>
2025-04-08 15:19:38 +0530

    Frontend is running smoothly 🚀 | All features working as expected ✅

--- src/app/user/login/page.jsx
+++ src/app/user/login/page.jsx
@@ -53,7 +53,6 @@ const Page = () => {
   const loginMutation = useMutation({
     mutationFn: userLogin,
     onSuccess: async (data) => {
-
       alert(data.data.message);
       form.reset();
       await fetchUser();
