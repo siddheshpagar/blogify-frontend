50f624263241606b8bcbb7728c300d3ed695542d
siddheshpagar <pagarsiddhesh2000@gmail.com>
2025-04-08 17:27:35 +0530

    update Frontend user/login/page.jsx

--- src/app/user/login/page.jsx
+++ src/app/user/login/page.jsx
@@ -56,7 +56,7 @@ const Page = () => {
       alert(data.data.message);
       form.reset();
       await fetchUser();
-      router.replace(redirectUrl);
+      
     },
     // 
     onError: (error) => {
@@ -68,6 +68,7 @@ const Page = () => {
   const handleSubmit = async (values) => {
     const { terms, ...loginData } = values;// Excluding terms field
     loginMutation.mutate(loginData);
+    router.push("/");
     // try {
     //   const response = await userLogin(loginData);
     //   if (response.status === 200) {
