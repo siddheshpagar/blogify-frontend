75c013425fbc449a79da59de598a30d85959b459
siddheshpagar <pagarsiddhesh2000@gmail.com>
2025-04-09 17:40:50 +0530

    Frontend is running smoothly 🚀 | All features working as expected ✅

--- src/app/admin/login/layout.js
+++ src/app/admin/login/layout.js
@@ -1,10 +1,18 @@
 "use client";
 
+import Image from "next/image";
 import { Suspense } from "react";
 
 export default function Layout({ children }) {
     return (
-        <Suspense fallback={<div>Loading...</div>}>
+        <Suspense fallback={
+            <div className="flex flex-col gap-2 items-center justify-center h-screen w-screen">
+                <Image className="animate-spin rounded-full" src="/loading.png" width={48} height={48} alt="Loading..." />
+                <p className="mt-4 text-2xl font-semibold text-white animate-pulse">
+                    Loading....
+                </p>
+            </div>
+        }>
             {children}
         </Suspense>
     );
